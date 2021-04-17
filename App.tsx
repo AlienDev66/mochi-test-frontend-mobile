a08cf85c4f3b83bcb99a38cbe1e4962260a7b77d
Domingos Capitango <p.capitango65@gmail.com>
2021-04-17 16:20:14 +0100

    :sparkles: Add App.tsx in the root.

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Teste from './app/components/teste';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('./assets/adaptive-icon.png')}
+                style={styles.logo}
+            />
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Teste />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 120,
+        height: 120,
+        margin: 10
+    }
+});
