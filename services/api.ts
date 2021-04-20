ab2b184e8af72b24f9d05fb0b6baf2ff1e7d98f9
Domingos Capitango <59874434+AlienDev66@users.noreply.github.com>
2021-04-20 17:57:51 +0100

    consume

---
+++ services/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const TOKEN_GITHUB_API = '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';
+
+const headers = {
+    Authorization: 'bearer ' + TOKEN_GITHUB_API
+};
+
+const api = axios.create({ headers, baseURL: 'https://api.github.com' });
+
+export default api;
